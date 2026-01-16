353e4e15ecd0af237a3bf2242b890343f5e8a2a6
yukta bande <yuktabande@yuktas-MacBook-Air.local>
2026-01-16 21:54:00 +0530

    comment feature + tag

--- django_backend/blog/models.py
+++ django_backend/blog/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    tags = models.ManyToManyField(Tag, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text[:30]
